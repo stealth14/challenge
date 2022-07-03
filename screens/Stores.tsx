cb7f57100c0f2c2d4a6f8f1c271ac6458abc849f
Ronny Cajas <ronny1418@hotmail.com>
2022-07-03 17:17:56 -0500

    bottom sheet added

--- screens/Stores.tsx
+++ screens/Stores.tsx
@@ -1,15 +1,48 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, SafeAreaView, Text, Button } from "react-native";
 import CustomMap from "../components/CustomMap";
+import { BottomSheet } from "react-native-btr";
 
 export default function HomeScreen() {
+  const [visible, setVisible] = useState(false);
+
+  const toggle = () => {
+    setVisible(!visible);
+  };
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <View style={styles.menu}></View>
       <View style={styles.map}>
         <CustomMap />
       </View>
-    </View>
+
+      <BottomSheet
+        visible={visible}
+        onBackButtonPress={toggle}
+        onBackdropPress={toggle}
+      >
+        <View style={styles.bottomNavigationView}>
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "column",
+              justifyContent: "space-between",
+            }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                padding: 20,
+                fontSize: 20,
+              }}
+            >
+              Share Using
+            </Text>
+            <View style={{ flex: 1, flexDirection: "row" }}></View>
+          </View>
+        </View>
+      </BottomSheet>
+    </SafeAreaView>
   );
 }
 
@@ -19,8 +52,17 @@ const styles = StyleSheet.create({
   },
   menu: {
     flex: 1,
+    backgroundColor: "red",
   },
   map: {
     flex: 5,
+    backgroundColor: "green",
+  },
+  bottomNavigationView: {
+    backgroundColor: "#fff",
+    width: "100%",
+    height: 250,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
